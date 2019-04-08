8d06f426b11eafe9e92befe5ddd17375b5f4b173
sajith priyankara <sajith1995s@gmail.com>
2019-04-08 20:22:29 +0530

    initial commit

---
+++ app/src/main/java/com/example/alarmsystem/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.alarmsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.test_btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+    }
+
+    public void openDialog() {
+        SetAlarm setAlarm = new SetAlarm();
+        setAlarm.show(getSupportFragmentManager(), "Set Alarm");
+    }
+}
---
+++ app/src/main/java/com/example/alarmsystem/SetAlarm.java
@@ -0,0 +1,42 @@
+package com.example.alarmsystem;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class SetAlarm extends AppCompatDialogFragment {
+
+    private DatePickerDialog time;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.set_alarm, null);
+
+        builder.setView(view)
+                .setTitle("Add Alarm")
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+        return builder.create();
+
+    }
+}
